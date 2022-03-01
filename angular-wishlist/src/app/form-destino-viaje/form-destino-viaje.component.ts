8fcf43e545cae7eb74c2db836a08eae19caaeb24
JeisonTrivi <jeisontrivino0617@gmail.com>
2022-02-28 20:01:55 -0500

    Version 1.0.5 Arquitectura App reactiva

--- angular-wishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ angular-wishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { DestinoViaje } from '../modelos/destinoViaje';
+import { fromEvent } from 'rxjs';
+import { map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+
+
 
 @Component({
   selector: 'app-form-destino-viaje',
@@ -12,9 +17,10 @@ export class FormDestinoViajeComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
 
   minLongitud = 3;
+  searchResults:any[];
 
-  fg: FormGroup= this.fb.group({
-    'nombre': ['', Validators.compose([      
+  fg: FormGroup = this.fb.group({
+    'nombre': ['', Validators.compose([
       Validators.required,
       this.nombreValidator,
       this.nombreValidadorParametrizable(this.minLongitud)
@@ -22,19 +28,31 @@ export class FormDestinoViajeComponent implements OnInit {
     'url': ['', Validators.required]
   });
 
-  constructor(private fb: FormBuilder) { 
-    this.onItemAdded = new EventEmitter(); 
-    this.fg.valueChanges.subscribe((form:any) =>{
+  constructor(private fb: FormBuilder) {
+    this.onItemAdded = new EventEmitter();
+    this.fg.valueChanges.subscribe((form: any) => {
       console.log(form)
     })
   }
-  
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+    const elemNombre = <HTMLInputElement>document.getElementById('nombre');
+    fromEvent(elemNombre, 'input')
+      .pipe(
+        map((e) => (e.target as HTMLInputElement).value),
+        filter(text => text.length > 2),
+        debounceTime(120),
+        distinctUntilChanged(),
+        switchMap(() => ajax('/assets/datos.json'))
+      ).subscribe(ajaxResponse => {        
+        console.log(ajaxResponse.response)
+        this.searchResults = ajaxResponse.response;
+      });
   }
 
-  guardar(nombre:string, url:string):boolean{
-    let d = new DestinoViaje (nombre, url);
+  guardar(nombre: string, url: string): boolean {
+    let d = new DestinoViaje(nombre, url);
     this.onItemAdded.emit(d);
     return false;
   }
@@ -43,13 +61,13 @@ export class FormDestinoViajeComponent implements OnInit {
    * @param control :FormControl
    * @returns 
    */
-  
+
   nombreValidator(control: FormControl): { [s: string]: boolean } | null {
     let l = control.value.toString().trim().length;
     if (l >= 0 && l < 5) {
-      return {invalidNombre: true};
+      return { invalidNombre: true };
     }
-      return {};
+    return {};
   }
   /**
    * 
@@ -57,14 +75,14 @@ export class FormDestinoViajeComponent implements OnInit {
    * @control :AbstractControl
    * @returns :ValidatorFn
    */
-  nombreValidadorParametrizable(minLong:number):ValidatorFn{
-    return(control: AbstractControl): { [s: string]: boolean } | null =>{
+  nombreValidadorParametrizable(minLong: number): ValidatorFn {
+    return (control: AbstractControl): { [s: string]: boolean } | null => {
       let l = control.value.toString().trim().length;
       if (l >= 0 && l < minLong) {
-        return {minLongNombre: true};
+        return { minLongNombre: true };
       }
       return null;
     }
   }
-  
 }
+
